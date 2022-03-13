051f47734cec0b88924c26be38500d985b9b1c00
堀航大 <kodenginner4521@gmail.com>
2022-03-13 14:13:08 +0900

    GetUser

--- main.go
+++ main.go
@@ -16,11 +16,14 @@ func main() {
 	fmt.Println(models.Db)
 
 	// u := &models.User{}
-	u := new(models.User)
-	u.Name = "test"
-	u.Email = "test@example.com"
-	u.PassWord = "testtest"
-	fmt.Println(u)
+	// u := new(models.User)
+	// u.Name = "test"
+	// u.Email = "test@example.com"
+	// u.PassWord = "testtest"
+	// fmt.Println(u)
 
-	u.CreateUser()
+	// u.CreateUser()
+
+	u, _ := models.GetUser(1)
+	fmt.Println(u)
 }
