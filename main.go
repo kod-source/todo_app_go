03597bda66be61b36524506bd7eb90aaaac0f9b7
堀航大 <kodenginner4521@gmail.com>
2022-03-13 12:25:41 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"todo_app/app/models"
+)
+
+func main() {
+	// fmt.Println(config.Config.Port)
+	// fmt.Println(config.Config.SQLDriver)
+	// fmt.Println(config.Config.DbName)
+	// fmt.Println(config.Config.LogFile)
+
+	// log.Println("test")
+
+	fmt.Println(models.Db)
+
+	// u := &models.User{}
+	u := new(models.User)
+	u.Name = "test"
+	u.Email = "test@example.com"
+	u.PassWord = "testtest"
+	fmt.Println(u)
+
+	u.CreateUser()
+}
