5d13018f123a7409a380ca0baae1b0322c512bb8
堀航大 <kodenginner4521@gmail.com>
2022-03-15 21:14:17 +0900

    server layoutの共通化

--- controllers/route_main.go
+++ controllers/route_main.go
@@ -1,15 +1,14 @@
 package controllers
 
 import (
-	"html/template"
-	"log"
 	"net/http"
 )
 
-func top(w http.ResponseWriter, r *http.Request)  {
-	t, err := template.ParseFiles("app/views/templates/top.html")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	t.Execute(w, "Hello")
+func top(w http.ResponseWriter, r *http.Request) {
+	// t, err := template.ParseFiles("app/views/templates/top.html")
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+	// t.Execute(w, "Hello")
+	generateHTML(w, "Hello", "layout", "top")
 }
