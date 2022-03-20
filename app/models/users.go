2584df60cd30a3b21ac99fad35a89a6b3d08835c
堀航大 <kodenginner4521@gmail.com>
2022-03-20 22:52:31 +0900

    logout

--- app/models/users.go
+++ app/models/users.go
@@ -135,3 +135,12 @@ func (session *Session) CheckSession() (valid bool, err error) {
 	}
 	return valid, err
 }
+
+func (session *Session) DeleteSessionByUUID() (err error) {
+	cmd := `delete from sessions where uuid = ?`
+	_, err = Db.Exec(cmd, session.UUID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
